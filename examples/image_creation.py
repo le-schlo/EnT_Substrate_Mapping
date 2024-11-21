3aeb5fb06e0ed5e7b74713aaf241e7d110ea1e3c
Leon Schlosser <92179005+le-schlo@users.noreply.github.com>
2024-11-21 14:49:28 +0100

    Update image_creation.py

--- examples/image_creation.py
+++ examples/image_creation.py
@@ -15,10 +15,10 @@ type_of_image = 'structure' # Options are structure or 'spin_population'
                             # Caveat of spin_population is that the memory for the generation for the images can overflow and crash the script.
                             # In this case the script needs to be run in smaller batches.
 
-data_path = home_dir + fr'\Data\virtual_libraries\{ring_class}_{substitution}.csv'
-picFolder_path = home_dir + rf'\Data\images'
+data_path = home_dir + f'/Data/virtual_libraries/{ring_class}_{substitution}.csv'
+picFolder_path = home_dir + '/Data/images'
 #Only required for spin_density images:
-sp_path = home_dir + fr'\Data\sp_predictions\predictions_{ring_class}_{substitution}.csv'
+sp_path = home_dir + f'/Data/sp_predictions/predictions_{ring_class}_{substitution}.csv'
 
 df = pd.read_csv(data_path)
 
@@ -35,7 +35,7 @@ if type_of_image == 'structure':
         idx = df[df['smiles'] == smi].index.to_numpy()[0]
         d.WriteDrawingText(f"{picFolder_path}/{idx}.jpeg")
 
-elif type_of_image == 'spin_density':
+elif type_of_image == 'spin_population':
     sp_data = pd.read_csv(sp_path)
     for i in range(len(sp_data)):
         smi = sp_data.True_smiles.to_list()[i]
